b154c51211a4f6592dd16f3cdbcb44bc452fdc98
Ivan Mayes <ivanmayes@gmail.com>
2017-12-05 11:39:47 -0600

    Basically built

--- server.js
+++ server.js
@@ -5,35 +5,23 @@ const express = require('express')
 const app     = express()
 const msg     = require('gulp-messenger')
 const chalk   = require('chalk')
-const Twig    = require('twig')
 const _       = require('lodash')
-const keypress = require('./lib/keypress');
+const mopidyApp = require('./lib/app');
+const io = require('socket.io');
 
-var twig = Twig.twig;
-
-// This section is optional and used to configure twig.
-app.set('twig options', {
-    strict_variables: false
-});
-
-app.set('port', (process.env.PORT || 3000))
-app.use(express.static(__dirname + '/public'))  // static directory
-
-// Custom routes (not convered by static files, uses twig template engine)
-app.get('/', (req, res) => {
-  res.render('index.twig', {
-    firstName: 'Mike',
-    lastName: 'Erickson'
-  });
-});
+app.set('port', (process.env.PORT || 3000));
 
 // lets startup this puppy
-app.listen(app.get('port'), () => {
+let server = app.listen(app.get('port'), () => {
   msg.log('\n')
   console.log(chalk.cyan('Server Started ' + new Date()));
   msg.log('\n')
   const serverInfo = chalk.yellow(`http://localhost:${app.get('port')}`);
   msg.success('=', _.pad(`Application Running On: ${serverInfo}`, 80), '=');
 
-  keypress.listen();
+  // Configure socket connection
+  let socket = require('./lib/io.js')(io, app, server);
+
+  // Start connection to mopidy
+  mopidyApp.listen();
 })
